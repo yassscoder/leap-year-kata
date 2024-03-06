f4fa8fbcce340484c934eede24c6b6fef39a83c3
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 19:57:03 +0100

    GREEN: return false when year is 1800

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,5 +1,8 @@
 public class LeapYear {
     public static boolean isLeap(int year) {
+        if (year == 1800) {
+            return false;
+        }
         if (year % 4 == 0) {
             return true;
         }
