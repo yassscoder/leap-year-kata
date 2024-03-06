e443028ef20945499b1c9db331565089fd5fd4fc
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 20:23:04 +0100

    refactor: switch if condition to simple return

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -6,9 +6,6 @@ public class LeapYear {
         if (year % 100 == 0) {
             return false;
         }
-        if (year % 4 == 0) {
-            return true;
-        }
-        return false;
+        return year % 4 == 0;
     }
 }
