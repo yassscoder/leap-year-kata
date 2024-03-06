27cdd8123366542b1d8ab9997717754f733cce7c
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 19:33:01 +0100

    GREEN: return true when divisible by 4

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,6 +1,6 @@
 public class LeapYear {
     public static boolean isLeap(int year) {
-        if (year == 1996) {
+        if (year % 4 == 0) {
             return true;
         }
         return false;
