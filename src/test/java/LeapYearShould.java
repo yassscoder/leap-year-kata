04a14109e1a66511b2d62b9c0a4a5449fe9b411b
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 18:53:56 +0100

    GREEN: should return false

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LeapYearShould {
+    @Test
+    @DisplayName("return false")
+    public void return_false(){
+        assertEquals(false, LeapYear.isLeap(1997));
+    }
+
+    private static class LeapYear {
+        public static boolean isLeap(int year) {
+            return false;
+        }
+    }
 }
